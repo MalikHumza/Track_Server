95627776311beccc9103744bdd6c26e99d2cfb62
MalikHumza <humzamalik1146@gmail.com>
2023-07-24 16:12:11 +0500

    MongoDB Setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const mongoUri = 'mongodb+srv://usr:usrmongo@trackcluster.cyfj7ak.mongodb.net/'
+
+mongoose.connect(mongoUri);
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('Error on Connecting MongoDB', err);
+});
 
 app.get('/', (req, res) => {
     res.send('Hi There!!');
