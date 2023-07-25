55bc52e3cfb6eb399c76280bc6ee058f1e62838c
MalikHumza <humzamalik1146@gmail.com>
2023-07-25 20:03:28 +0500

    UserSchema

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
+const User = mongoose.model('User');
 
 router.post('/signup', (req, res) => {
     console.log(req.body);
