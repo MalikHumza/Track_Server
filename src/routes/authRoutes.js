08bcb4d382964b5012d8529cb75237634bf9f854
MalikHumza <humzamalik1146@gmail.com>
2023-07-25 20:08:39 +0500

    createUser

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 const router = express.Router();
 const User = mongoose.model('User');
 
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     console.log(req.body);
+    const {email, password} = req.body;
+    const user = new User({email, password});
+    await user.save();
     res.send('Hello, you made a post request.');
 })
 
