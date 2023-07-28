3528f65fcc54b7e38193674ab51b5932fdb4dea2
MalikHumza <humzamalik1146@gmail.com>
2023-07-28 16:54:59 +0500

    Handling

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,8 +6,13 @@ const User = mongoose.model('User');
 router.post('/signup', async (req, res) => {
     console.log(req.body);
     const {email, password} = req.body;
-    const user = new User({email, password});
-    await user.save();
+    try{
+        const user = new User({email, password});
+        await user.save();
+    } catch(err) {
+        return res.status(422).send(err.message);
+    }
+    
     res.send('Hello, you made a post request.');
 })
 
